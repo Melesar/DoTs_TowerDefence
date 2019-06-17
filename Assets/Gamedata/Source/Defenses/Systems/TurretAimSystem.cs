b6c4f7b9fc06beeb4c13899ed3146eefb02666e5
s.pikhota <s.pikhota@corp.nekki.ru>
2019-06-17 15:00:19 +0200

    Improved aiming

--- Assets/Gamedata/Source/Defenses/Systems/TurretAimSystem.cs
+++ Assets/Gamedata/Source/Defenses/Systems/TurretAimSystem.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
@@ -11,9 +12,13 @@ namespace DoTs
     [UpdateBefore(typeof(TurretRotationSystem))]
     public class TurretAimSystem : ComponentSystem
     {
+        private NativeList<JobHandle> _handles;
+        
         protected override void OnUpdate()
         {
-            Entities.ForEach((Entity entity, ref TurretAim aim, ref TurretRotation rotationData, ref Translation translation) =>
+            var index = 0;
+            Entities.WithAllReadOnly<TurretAim, Translation>()
+                .ForEach((Entity entity, ref TurretAim aim, ref Translation translation) =>
             {
                 var foundEnemies = new NativeArray<float3>(1, Allocator.TempJob);
                 var hasFoundEnemies = new NativeArray<bool>(1, Allocator.TempJob);
@@ -24,32 +29,29 @@ namespace DoTs
                     foundEnemies = foundEnemies,
                     indicator = hasFoundEnemies
                 };
-                
-                findTargetJob.Schedule(this).Complete();
 
                 var hasTarget = EntityManager.HasComponent<TargetOwnership>(entity);
-                if (hasTarget && hasFoundEnemies[0])
+                var updateTargetJob = new UpdateTargetJob
                 {
-                    PostUpdateCommands.SetComponent(entity, new TargetOwnership {targetPosition = foundEnemies[0]});
-                }
-                else if (hasTarget)
-                {
-                    PostUpdateCommands.RemoveComponent<TargetOwnership>(entity);
-                }
-                else if (hasFoundEnemies[0])
-                {
-                    PostUpdateCommands.AddComponent(entity, new TargetOwnership {targetPosition = foundEnemies[0]});
-                }
-                
-                foundEnemies.Dispose();
-                hasFoundEnemies.Dispose();
-
+                    index = index,
+                    entity = entity,
+                    hasTarget = hasTarget,
+                    commandBuffer = PostUpdateCommands.ToConcurrent(),
+                    enemyPositions = foundEnemies,
+                    hasFoundEnemies = hasFoundEnemies,
+                };
                 var rotateTurretsJob = new RotateTowardsTargetJob();
-                rotateTurretsJob.Schedule(this).Complete();
-            });
+  
+                var handle = findTargetJob.Schedule(this);
+                handle = updateTargetJob.Schedule(handle);
+                handle = rotateTurretsJob.Schedule(this, handle);
 
+                AddHandle(index++, handle);
+            });
+            
+            JobHandle.CompleteAll(_handles.AsArray());
         }
-        
+
         [RequireComponentTag(typeof(Enemy))]
         [BurstCompile]
         private struct FindEnemiesWithinRangeJob : IJobForEachWithEntity<Translation>
@@ -78,6 +80,35 @@ namespace DoTs
             }
         }
 
+        private struct UpdateTargetJob : IJob
+        {
+            public int index;
+            public Entity entity;
+            public bool hasTarget;
+            public EntityCommandBuffer.Concurrent commandBuffer;
+            
+            [ReadOnly, DeallocateOnJobCompletion] 
+            public NativeArray<float3> enemyPositions;
+            [ReadOnly, DeallocateOnJobCompletion] 
+            public NativeArray<bool> hasFoundEnemies;
+            
+            public void Execute()
+            {
+                if (hasTarget && hasFoundEnemies[0])
+                {
+                    commandBuffer.SetComponent(index, entity, new TargetOwnership {targetPosition = enemyPositions[0]});
+                }
+                else if (hasTarget)
+                {
+                    commandBuffer.RemoveComponent<TargetOwnership>(index, entity);
+                }
+                else if (hasFoundEnemies[0])
+                {
+                    commandBuffer.AddComponent(index, entity, new TargetOwnership {targetPosition = enemyPositions[0]});
+                }
+            }
+        }
+
         [BurstCompile]
         private struct RotateTowardsTargetJob : IJobForEach<TargetOwnership, TurretRotation, Rotation, Translation>
         {
@@ -93,6 +124,28 @@ namespace DoTs
                 rotationData.isTurning = true;
                 rotationData.targetAngle = targetQuaternion.eulerAngles.z;
             }
+        }
+
+        private void AddHandle(int index, JobHandle handle)
+        {
+            if (_handles.Length <= index)
+            {
+                _handles.Add(handle);
+            }
+            else
+            {
+                _handles[index] = handle;
+            }
+        }
+
+        protected override void OnCreate()
+        {
+            _handles = new NativeList<JobHandle>(Allocator.Persistent);
+        }
+
+        protected override void OnDestroy()
+        {
+            _handles.Dispose();
         }
     }
 }
